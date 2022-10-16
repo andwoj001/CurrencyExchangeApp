bb2c7ae04198282a2e8de45f2293f01a7dfebfb9
Andrzej Wojciechowski <andwoj001@gmail.com>
2022-10-16 17:19:15 +0200

    Menu class added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,7 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!" + "\n" + "Enter the integer number to be multiplied by itself:");
-        Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
-        System.out.println("The result of multiplying this integer number by itself is:" + "\n" + Multiplication.multiplication(number));
+        Menu.mainMenu();
     }
 }
--- src/main/java/org/example/Multiplication.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Multiplication {
-    public static int multiplication(int x) {
-        return x * x;
-    }
-}
